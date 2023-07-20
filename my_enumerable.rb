f8d7542ea2709fde81d5a3564a3adad83c370466
kifle23 <kiflekifle23@gmail.com>
2023-07-20 21:05:02 +0300

    Fix linter

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,18 +1,17 @@
 module MyEnumerable
-    def all?(&block)
-      each { |item| return false unless block.call(item) }
-      true
-    end
-  
-    def any?
-      each { |item| return true if yield(item) }
-      false
-    end
-  
-    def filter(&block)
-      filtered_array = []
-      each { |element| filtered_array << element if block.call(element) }
-      filtered_array
-    end
+  def all?(&block)
+    each { |item| return false unless block.call(item) }
+    true
   end
-  
+
+  def any?
+    each { |item| return true if yield(item) }
+    false
+  end
+
+  def filter(&block)
+    filtered_array = []
+    each { |element| filtered_array << element if block.call(element) }
+    filtered_array
+  end
+end
